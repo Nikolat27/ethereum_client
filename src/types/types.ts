1fe4b9228e343603f66aac70d1722f608f62690f
Sam <samalizadeh899@gmail.com>
2025-12-27 17:14:49 +0330

    Generalized the select items

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,8 @@
 export type WalletType = "private-key" | "mnemonic";
 
-export type NetworkItem = {
+export type SelectItem = {
     value: string;
     text: string;
 };
 
-export type NetworkItemsList = Array<NetworkItem>;
+export type SelectItemsList = Array<SelectItem>;
